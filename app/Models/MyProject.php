33a18b7c4b53c9ad4ecaa6999a39a822dc345b1f
AngeloCrisci <angelocrisci1998@gmail.com>
2024-11-05 16:11:33 +0100

    Fillable

--- app/Models/MyProject.php
+++ app/Models/MyProject.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MyProject extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "date",
+        "description",
+        "link",
+    ];
 }
